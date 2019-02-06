7abbe46eeeafaf360ebb4836696b35e712dff237
Ange09_ <geraldodeveloper09@gmail.com>
2019-02-05 21:39:00 -0400

    Comienzo sección principal
    Creo la sección de 'Sobre mí' y mis habilidades.

--- portafolio-es/js/main.js
+++ portafolio-es/js/main.js
@@ -1,10 +1,29 @@
 $('document').ready(function(){
 
+    //ANIMACIÓN DE "REVELACIÓN" DE TEXTO.
+
+    /* Guardo en la variable "profesion" la etiqueta que contiene el texto 
+    al cual quiero aplicar el efecto de revelado.*/
     const profesion = baffle(".profesion");
+
+    /* Le asigno a la variable profesión (que contiene el texto que quiero animar) los caracteres a utilizar para 
+    la animación del texto revelado (puedes utilizar cualquier tipo de carácteres). También asigno la velocidad que tendrá el efecto de las letras*/
     profesion.set({
         characters: '█▓▓ ░░>██ ▓█▓>▓ ▓<█ ░<▒░▓ █░<█ █▒> ▓░▓< ▒▓░░',
-        speed: 120
+        speed: 90
     });
+
+    // Ejecuto el inicio de la animacion.
     profesion.start();
-    profesion.reveal(2000);
+    // Establezco la duración que tendrá la animación antes de revelar el texto.
+    profesion.reveal(3000);
+    
+    $('#tagline').t({
+        beep:false,
+        caret:'<span style="color:hotpink;">•</span>',
+        typing:function(elm,chr){
+        if(chr.match(/\-trigger/))
+            $('#pow-txt').show().delay(500).fadeOut(0);
+        }
+    });
 });
